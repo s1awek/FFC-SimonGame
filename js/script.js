ff978aaa9418f98c76819bb26a25e36a01d7cc8e
s1awek <elvizky@gmail.com>
2017-04-05 14:11:29 +0200

    Code refactoring and added some basic funcinality

--- js/script.js
+++ js/script.js
@@ -4,30 +4,58 @@ $(document).ready(function () {
     var tempPlayerMoves = [];
     var currLength = 0;
     var s1, s2, s3, s4;
-    //var s3 = new Audio('sounds/simonSound3.mp3');
-    //var s4 = new Audio('sounds/simonSound4.mp3');
     var buzzer = new Audio('sounds/buzzer.mp3');
+    var checked = false;
+    var start = false;
+    var strict = false;
+    var gameArr = [];
+    var obj;
 
+    function createGame() {
+        for (var i = 0; i < 20; i = i + 1) {
+            var x = Math.floor((Math.random() * 4) + 1);
+            gameArr.push('s' + x);
+        }
+    }
+
+    function computerTurn() {
+
+    }
+
+    function playNow(color, audio, s) {
+        if (s === 's1') {
+            obj = '#inner-1';
+        } else if (s === 's2') {
+            obj = '#inner-2';
+        } else if (s === 's3') {
+            obj = '#inner-3';
+        } else if (s === 's4') {
+            obj = '#inner-4';
+        }
+        if (start) {
+            $(obj).css('background-color', color);
+            //Push player's move to playerMoves array
+            playerMoves.push(s);
+            //Create sound object
+            s = new Audio(audio);
+            //Trigger function to compare with gameArr
+            compareMoves();
+            //Play sound
+            s.play();
+        }
+    }
+
+    //Add highlight and sound when pressed
     $('#inner-1').on('mousedown', function () {
-        $(this).css('background-color', '#00ff6e');
-        currLength = currLength + 1
-        s1 = new Audio('sounds/simonSound1.mp3');
-        playerMoves.push('s1');
-        compareMoves();
-        s1.play();
+        playNow('#00ff6e', 'sounds/simonSound1.mp3', 's1');
     });
-
+    //Back to default state when moseover on mouse up
     $('#inner-1').on('mouseout mouseup', function () {
         $(this).css('background-color', '#00A74A');
     });
 
     $('#inner-2').on('mousedown', function () {
-        $(this).css('background-color', '#fc2530');
-        currLength = currLength + 1
-        s2 = new Audio('sounds/simonSound2.mp3');
-        playerMoves.push('s2');
-        compareMoves();
-        s2.play();
+        playNow('#fc2530', 'sounds/simonSound2.mp3', 's2');
     });
 
     $('#inner-2').on('mouseout mouseup', function () {
@@ -35,12 +63,7 @@ $(document).ready(function () {
     });
 
     $('#inner-3').on('mousedown', function () {
-        $(this).css('background-color', '#ffd014');
-        currLength = currLength + 1;
-        s3 = new Audio('sounds/simonSound3.mp3');
-        playerMoves.push('s3');
-        compareMoves();
-        s3.play();
+        playNow('#ffd014', 'sounds/simonSound3.mp3', 's3');
     });
 
     $('#inner-3').on('mouseout mouseup', function () {
@@ -48,12 +71,7 @@ $(document).ready(function () {
     });
 
     $('#inner-4').on('mousedown', function () {
-        $(this).css('background-color', '#168aff');
-        currLength = currLength + 1
-        s4 = new Audio('sounds/simonSound4.mp3');
-        playerMoves.push('s4');
-        compareMoves();
-        s4.play();
+        playNow('#168aff', 'sounds/simonSound4.mp3', 's4');
     });
 
     $('#inner-4').on('mouseout mouseup', function () {
@@ -64,5 +82,42 @@ $(document).ready(function () {
     function compareMoves() {
         console.log(playerMoves);
     }
+
+    //Reads controls states and set it to true or false
+    $('#start').on('click', function () {
+        if (checked) {
+            $(this).toggleClass('active');
+            if ($(this).hasClass('active')) {
+                createGame();
+                computerTurn();
+                playerMoves = [];
+                start = true;
+            } else {
+                start = false;
+            }
+        }
+    });
+
+    $('#strict').on('click', function () {
+        if (checked) {
+            $(this).toggleClass('active');
+            if ($(this).hasClass('active')) {
+                strict = true;
+            } else {
+                strict = false;
+            }
+        }
+    });
+
+    $('#checkbox').change(function () {
+        if ($(this).is(':checked')) {
+            checked = true;
+        } else {
+            $('#start').removeClass('active');
+            start = false;
+            checked = false;
+        }
+    });
+
 
 });
